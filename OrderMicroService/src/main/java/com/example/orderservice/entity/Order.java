bcf1dab4eabe2f833e1008d43f1f389df9e0c346
nafiz1307 <nafizzamanarnob17@gmail.com>
2022-03-23 12:21:29 +0600

    implementation complete

---
+++ OrderMicroService/src/main/java/com/example/orderservice/entity/Order.java
@@ -0,0 +1,32 @@
+package com.example.orderservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Order {
+    @Id
+    private String id;
+    @Field
+    private String date;
+    @Field
+    private String customerId;
+    @Field
+    private String productId;
+    @Field
+    private String shopId;
+    @Field
+    private String orderItems;
+    @Field
+    private String orderQuantity;
+
+
+}
